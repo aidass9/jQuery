dcb9f80cfe8beed04d3c9278e8b703056a3e2585
Aida <aida9.soriano@gmail.com>
2018-01-30 12:31:00 +0100

    204a

---
+++ 204a/204a.js
@@ -0,0 +1,13 @@
+$(function() {
+    // $("body>ul ul").css("display", "none");
+    $("body>ul ul").hide();
+
+    $("body>ul>li").mouseenter(function() {
+        $(this).find("ul").slideDown();
+    })
+
+    $("body>ul>li").mouseleave(function() {
+        $(this).find("ul").slideUp();
+    })
+})
+
